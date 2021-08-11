41ece0e9b1939e2f06291633eaba149aa2aa7610
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-08-11 11:00:15 +0200

    Tests fps with cubes

---
+++ Assets/WorldController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+    public GameObject block;
+    public int worldWidth = 10;
+    public int worldHeight = 2;
+    public int worldDepth = 10;
+
+    public IEnumerator BuildWorld() {
+        for (int z = 0; z < worldDepth; z++)
+        for (int y = 0; y < worldHeight; y++) {
+            for (int x = 0; x < worldWidth; x++) {
+                Vector3 pos = new Vector3(x,y,z);
+                GameObject cube = GameObject.Instantiate(block, pos, Quaternion.identity);
+                cube.name = x + "_" + y + "_" + z;
+                cube.GetComponent<Renderer>().material = new Material(Shader.Find("Standard"));
+            }
+
+            yield return null;
+        }    
+        
+    }
+    void Start() {
+        StartCoroutine(BuildWorld());
+    }
+
+}
